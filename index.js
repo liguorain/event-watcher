8e1b9b902cf0b89b10bd712e9163ecd6efee2b0c
liguorain <55340469+liguorain@users.noreply.github.com>
2020-04-18 09:02:03 +0800

    Add initiator function .
    先前的版本只能创建一个全局有效的事件总线，这样的设计不是非常友好：对于复杂的应用而言，开发者容易忘了哪些事件 type 已经被使用。本次更新新增了 `eventWatcher` 方法，可以使用此方法创建一个作用域有限的事件总线，不必再绞尽脑汁避免事件类型冲突了。
    
    The version before this commit can only create one global event watcher, developers should manage event types very carefull in a complex application .After this commit ,you could create a scoped event watcher by the function named `'eventWatcher'`, dont worry about the compatible, i've solved it.

--- index.js
+++ index.js
@@ -6,110 +6,123 @@
  *
  */
 
-/**
- * eventSet - 注册事件的回调合集
- * @type {Object}
- */
-const eventSet = {
-  authorize: []
-};
-
-/**
- * @const eventBuf - 对于监听前就可能发生的事件，缓存事件，设置监听时即触发回调
- * @type {Object}
- */
-const eventBuf = {};
-
-/**
- * @function emit - 发送全局事件
+/***
+ * @function eventWatcher - 事件总线的初始化函数
+ * @returns {{one: one, clearBuffer: clearBuffer, emit: emit, off: off, on: (function(String, Function, (Boolean|*)=): *)}}
  *
- * @param {String} type - 事件类型
- * @param {Object} event - 事件对象
- * @param {Boolean} [buffer = false] 是否缓存事件值
- * 
+ * TODO 提取所有子方法，以减少对内存的消耗
  */
-const emit = (type, event, buffer = false) => {
-  const eventCopy = JSON.parse(JSON.stringify(event));
+const eventWatcher = () => {
+    /**
+     * eventSet - 注册事件的回调合集
+     * @type {Object}
+     */
+    const eventSet = {
+        authorize: []
+    };
 
-  Object.freeze(eventCopy);
+    /**
+     * @const eventBuf - 对于监听前就可能发生的事件，缓存事件，设置监听时即触发回调
+     * @type {Object}
+     */
+    const eventBuf = {};
 
-  buffer && (eventBuf[type] = eventCopy);
+    /**
+     * @function emit - 发送全局事件
+     *
+     * @param {String} type - 事件类型
+     * @param {Object} event - 事件对象
+     * @param {Boolean} [buffer = false] 是否缓存事件值
+     *
+     */
+    const emit = (type, event, buffer = false) => {
+        const eventCopy = JSON.parse(JSON.stringify(event));
 
-  if (eventSet[type]) {
-    eventSet[type].forEach(item => {
-      item && item(eventCopy);
-    });
-  }
+        Object.freeze(eventCopy);
+
+        buffer && (eventBuf[type] = eventCopy);
+
+        if (eventSet[type]) {
+            eventSet[type].forEach(item => {
+                item && item(eventCopy);
+            });
+        }
+    };
+
+    /**
+     * @function on - 注册全局事件
+     *
+     * @param {String} type - 事件类型
+     * @param {Function} callback - 事件回调函数
+     * @param {Boolean|*} [useBuffer = false] - 事件
+     *
+     * @throws 对 callback 函数的类型检查
+     *
+     * @returns {Number} 事件句柄，用于取消事件监听
+     */
+    const on = (type, callback, useBuffer = false) => {
+        if (!eventSet[type]) {
+            eventSet[type] = [];
+        }
+
+        if (!callback instanceof Function) {
+            throw new Error('callback must be a Function!');
+        }
+
+        useBuffer && eventBuf[type] && callback(eventBuf[type]);
+
+        eventSet[type].push(callback);
+
+        return eventSet[type].length
+    };
+
+    /**
+     * @function off - 取消对某事件的监听
+     *
+     * @param {String} type - 事件类型
+     * @param {Number} id - 需要取消的事件ID，即 registEvent 所返回的值
+     *
+     * @returns {Null}
+     */
+    const off = (type, id) => {
+        if (!eventSet[type]) return null
+
+        if (eventSet[type][id - 1]) {
+            eventSet[type][id - 1] = false
+        }
+
+        if (!eventSet[type].reduce((pre, cur) => cur || pre, false)) {
+            eventSet[type] = []
+        }
+    };
+
+    /**
+     * @const one - 只订阅一次的事件
+     *
+     * @param {String} type - 订阅的事件类型
+     * @param {Function} callback - 事件回调
+     * @param {Boolean} [useBuffer = false] - 是否使用缓存的事件
+     */
+    const one = (type, callback, [useBuffer = false]) => {
+        const handler = on(type, e => {
+            callback(e);
+            off(type, handler);
+        }, useBuffer);
+    };
+
+    /**
+     * @const clearBuffer - 清空指定类型的事件缓存
+     *
+     * @param {String} type - 事件类型
+     */
+    const clearBuffer = type => {
+        delete eventBuf[type];
+    };
+
+    return { emit, on, off, one, clearBuffer }
 };
 
-/**
- * @function on - 注册全局事件
- * 
- * @param {String} type - 事件类型
- * @param {Function} callback - 事件回调函数
- * @param {Boolean|*} [useBuffer = false] - 事件
- *
- * @throws 对 callback 函数的类型检查
- * 
- * @returns {Number} 事件句柄，用于取消事件监听
- */
-const on = (type, callback, useBuffer = false) => {
-  if (!eventSet[type]) {
-    eventSet[type] = [];
-  }
+// 初始化一个全局事件总线，以向后兼容
+const globalEventWatcher = eventWatcher();
 
-  if (!callback instanceof Function) {
-    throw new Error('callback must be a Function!');
-  }
-
-  useBuffer && eventBuf[type] && callback(eventBuf[type]);
-
-  eventSet[type].push(callback);
-
-  return eventSet[type].length
-};
-
-/**
- * @function off - 取消对某事件的监听
- *
- * @param {String} type - 事件类型 
- * @param {Number} id - 需要取消的事件ID，即 registEvent 所返回的值
- * 
- * @returns {Null}
- */
-const off = (type, id) => {
-  if (!eventSet[type]) return null
-
-  if (eventSet[type][id - 1]) {
-    eventSet[type][id - 1] = false
-  }
-
-  if (!eventSet[type].reduce((pre, cur) => cur || pre, false)) {
-    eventSet[type] = []
-  }
-};
-
-/**
- * @const one - 只订阅一次的事件
- * 
- * @param {String} type - 订阅的事件类型
- * @param {Function} callback - 事件回调
- * @param {Boolean} [useBuffer = false] - 是否使用缓存的事件
- */
-const one = (type, callback, [useBuffer = false]) => {
-  const handler = on(type, e => {
-    callback(e);
-    off(type, handler);
-  }, useBuffer);
-};
-
-/**
- * @const clearBuffer - 清空指定类型的事件缓存
- * 
- * @param {String} type - 事件类型
- */
-const clearBuffer = type => {
-  delete eventBuf[type];
-};
-
-export { emit, on, off, one, clearBuffer }
+export {...globalEventWatcher, eventWatcher};
